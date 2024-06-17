797753b01af4e4dd1403a59b20d3c3c4da116a1f
PANKAJ MANRAL <144159258+pankajmanral@users.noreply.github.com>
2024-06-17 22:35:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".linkedin").addEventListener("click",function(){
+    window.open("https://www.linkedin.com/in/pankaj-manral-028190251/");
+})
+
+document.querySelector(".github").addEventListener("click",function(){
+    window.open("https://github.com/pankajmanral");
+})
+
+document.querySelector(".gmail").addEventListener("click",function(){
+    window.open("https://mail.google.com/mail/u/0/#inbox?compose=new")
+})
+
+document.querySelector(".link").addEventListener("click",function(){
+    window.open("https://www.linkedin.com/in/pankaj-manral-028190251/");
+})
